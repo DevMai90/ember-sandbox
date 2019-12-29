8f32e93157d0f2e74c2f27430cadb4eda8389a06
DevMai90 <dmai90@yahoo.com>
2019-12-29 02:30:06 -0800

    Render about template on bookmarks/index route. Assign attributes for bookmark model

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -1,3 +1,11 @@
 import DS from "ember-data";
+import attr from "ember-data/attr";
 
-export default DS.Model.extend({});
+export default DS.Model.extend({
+  // Define each attribute that is available
+  link: attr(),
+  title: attr(),
+  about: attr(),
+  public: attr(),
+  created: attr()
+});
--- app/routes/bookmarks/index.js
+++ app/routes/bookmarks/index.js
@@ -1,4 +1,11 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
+  model() {
+    return this.get("store").findAll("bookmark");
+  },
+  renderTemplate() {
+    // Tells Ember to render this certain template instead
+    this.render("about");
+  }
 });
