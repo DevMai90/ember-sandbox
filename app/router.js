4b0dd6f4d54569de461f82abbafc46e52fbb2290
DevMai90 <dmai90@yahoo.com>
2019-12-28 18:04:02 -0800

    Create dynamic route to edit bookmarks and a wildcard route

--- app/router.js
+++ app/router.js
@@ -1,13 +1,27 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
+// Manages our routes!
 Router.map(function() {
-  this.route('about');
+  this.route("about");
+  // May also use alias
+  // this.route("about", { path: "/aboutus" });
+  this.route("bookmarks", function() {
+    this.route("new");
+
+    this.route("edit", {
+      path: "/edit/:bookmark_id"
+    });
+  });
+  // All routes not controlled by Ember will default to this path
+  this.route("page-not-found", {
+    path: "/*wildcard"
+  });
 });
 
 export default Router;
